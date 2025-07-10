27d6bd6a39651502f9339db795688750a5a5a22e
rff ald <117867334+xiv3r@users.noreply.github.com>
2025-07-10 08:56:06 +0800

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+# Dependencies
+sudo apt update && sudo apt upgrade -y
+sudo apt install iptables dsniff ipcalc -y
+
+# Repo
+git clone https://github.com/xiv3r/netkiller.git
+cd netkiller
