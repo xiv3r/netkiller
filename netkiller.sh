f988bacf38a748c209efbf7a8867c9b6a24c8d2f
rff ald <117867334+xiv3r@users.noreply.github.com>
2025-07-12 20:07:56 +0800

    Update netkiller.sh

--- netkiller.sh
+++ netkiller.sh
@@ -34,7 +34,7 @@ for TARGET in $TARGET_IPS; do
    
     # Block all traffic of the target wifi clients
     iptables -I FORWARD -s "$TARGET" -d "$GATEWAY" -j DROP
-    iptables -I FORWARD -d "$GATEWAY" "$TARGET -j DROP
+    iptables -I FORWARD -d "$GATEWAY" -s "$TARGET -j DROP
     iptables -t nat -I PREROUTING -s "$TARGET" -j DNAT --to-destination "$GATEWAY"
    
     # Uncomment to Block all the traffic
