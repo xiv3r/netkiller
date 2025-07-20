8d5cc1c48fe0193454903153154ad5c3798039e5
rff ald <117867334+xiv3r@users.noreply.github.com>
2025-07-20 17:07:21 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Dependencies
-sudo apt install git iptables dsniff ipcalc iproute2 -y
+sudo apt install git arp-scan iptables dsniff ipcalc iproute2 -y
 
 # Repo
 git clone https://github.com/xiv3r/netkiller.git
