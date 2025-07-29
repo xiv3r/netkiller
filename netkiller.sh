d4751d93b5eddf0b830f09f0560681df83a8087d
rff ald <117867334+xiv3r@users.noreply.github.com>
2025-07-29 20:50:13 +0800

    Update netkiller.sh

--- netkiller.sh
+++ netkiller.sh
@@ -124,13 +124,6 @@ case $target_type in
             exit 1
         fi
 
-        # Ip forwarding
-        echo 1 > /proc/sys/net/ipv4/ip_forward
-
-        # iptables policy 
-        iptables -P FORWARD DROP
-        iptables -t mangle -I PREROUTING -i "$INTERFACE" -j TTL --ttl-set 0
-
         # Get network range using ipcalc
         NETWORK_INFO=$(ipcalc -n "$subnet")
         NETWORK_ADDR=$(echo "$NETWORK_INFO" | grep 'Network:' | awk '{print $2}' | cut -d'/' -f1)
